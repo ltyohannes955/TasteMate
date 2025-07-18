b123298fdb42f457e66710560a9b4b1bf1071af9
ltyohannes955 <ltyohannes95@gmail.com>
2025-07-18 22:43:10 +0300

    created layout for the app with a bottom nav bar and a title for each page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,31 +1,96 @@
-import Ionicons from "@expo/vector-icons/Ionicons";
+import type { BottomTabBarProps } from "@react-navigation/bottom-tabs";
 import { Tabs } from "expo-router";
+import { Calendar, Home, Menu, Search } from "lucide-react-native";
+import { Pressable, Text, View } from "react-native";
+
+function MyTabBar({ state, descriptors, navigation }: BottomTabBarProps) {
+  return (
+    <View className="flex-row bg-[#281c1b] h-20">
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const isFocused = state.index === index;
+        const label =
+          typeof options.tabBarLabel === "function"
+            ? options.tabBarLabel({
+                focused: isFocused,
+                color: isFocused ? "#1e40af" : "#6b7280",
+                position: "below-icon",
+                children: route.name,
+              })
+            : (options.tabBarLabel ?? options.title ?? route.name);
+
+        const Icon =
+          route.name === "home/index"
+            ? Home
+            : route.name === "search/index"
+              ? Search
+              : route.name === "plan/index"
+                ? Calendar
+                : Menu;
+
+        return (
+          <Pressable
+            key={route.key}
+            onPress={() => navigation.navigate(route.name)}
+            className={`flex-1 items-center justify-center gap-1`}
+          >
+            <Icon
+              size={24}
+              color={isFocused ? "white" : "#6b7280"}
+              className="mb-1"
+            />
+            <Text
+              className={`text-xs ${isFocused ? "text-white" : "text-gray-500"}`}
+            >
+              {label}
+            </Text>
+          </Pressable>
+        );
+      })}
+    </View>
+  );
+}
+
 export default function TabLayout() {
   return (
     <Tabs
+      tabBar={(props) => <MyTabBar {...props} />}
       screenOptions={{
-        tabBarActiveTintColor: "#ffd33d",
+        headerShown: true,
+        headerTitleAlign: "center",
         headerStyle: {
-          backgroundColor: "#25292e",
+          backgroundColor: "#181110", // match your tab bar
         },
-        headerShadowVisible: false,
-        headerTintColor: "#fff",
-        tabBarStyle: {
-          backgroundColor: "#25292e",
+        headerTitleStyle: {
+          color: "white",
+          fontSize: 18,
+          fontWeight: "bold",
         },
+        headerTintColor: "white", // color for icons/buttons
       }}
     >
       <Tabs.Screen
-        name="index"
+        name="home/index"
         options={{
           title: "Home",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={focused ? "home-sharp" : "home-outline"}
-              color={color}
-              size={24}
-            />
-          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search/index"
+        options={{
+          title: "Discover",
+        }}
+      />
+      <Tabs.Screen
+        name="plan/index"
+        options={{
+          title: "Plan",
+        }}
+      />
+      <Tabs.Screen
+        name="more/index"
+        options={{
+          title: "More",
         }}
       />
     </Tabs>
