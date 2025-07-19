8bca6c9e8bcfe4661a707f92a230eb501a8b4f97
ltyohannes955 <ltyohannes95@gmail.com>
2025-07-19 17:09:17 +0300

    added components and also title and view all button to the home page

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,9 +1,39 @@
-import { Text, View } from "react-native";
+import MovieSlider from "@/app/components/movieSlider";
+import TvShowSlider from "@/app/components/tvShowSlider";
+import { router } from "expo-router";
+import { SafeAreaView, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-[#181110]">
-      <Text className="text-lg font-bold text-blue-600">Welcome to Home</Text>
-    </View>
+    <SafeAreaView className="flex-1 items-center bg-[#181110] gap-5 px-5">
+      <ScrollView
+        className="flex-1"
+        contentContainerStyle={{ paddingVertical: 20, gap: 20 }}
+        showsVerticalScrollIndicator={false}
+      >
+        <View className="flex flex-row justify-between w-full items-end">
+          <Text className="text-white text-3xl font-bold">Popular Movies</Text>
+          <Text
+            className="text-white"
+            onPress={() => router.navigate("/(tabs)/movies")}
+          >
+            View all
+          </Text>
+        </View>
+        <MovieSlider />
+        <View className="flex flex-row justify-between w-full items-end">
+          <Text className="text-white text-3xl font-bold">
+            Popular Tv Shows
+          </Text>
+          <Text
+            className="text-white"
+            onPress={() => router.navigate("/(tabs)/tvShows")}
+          >
+            View all
+          </Text>
+        </View>
+        <TvShowSlider />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
